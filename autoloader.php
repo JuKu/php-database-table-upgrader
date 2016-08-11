60d59b252e2359f791db3b01c960bda07553cdf5
JuKu <kuenzel.justin@t-online.de>
2016-08-11 20:08:31 +0200

    added index.php.

---
+++ autoloader.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Justin
+ * Date: 11.08.2016
+ * Time: 20:07
+ */
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file was only created for test purposes!
+ * You dont need this file in production!
+ */
+
+?>
