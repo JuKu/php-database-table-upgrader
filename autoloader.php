148d00a7b254738bddf382834fdc3f324d16cce7
JuKu <kuenzel.justin@t-online.de>
2016-08-11 20:16:50 +0200

    change index.php, added support for autoloader.

--- autoloader.php
+++ autoloader.php
@@ -1,10 +1,25 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: Justin
- * Date: 11.08.2016
- * Time: 20:07
+ * Autoloader to include classes automatically, if they are required
  */
+
+//define ROOT_PATH if neccessary
+if (!defined("DTU_ROOT_PATH")) {
+    define('DTU_ROOT_PATH', dirname(__FILE__) . "/");
+}
+
+//function to auto load classes
+function loadClass ($class_name) {
+    //convert to lower case
+    $class_name = strtolower($class_name);
+
+    if (file_exists(DTU_ROOT_PATH . "classes/" . $class_name . ".php")) {
+        require(DTU_ROOT_PATH . "classes/" . $class_name . ".php");
+    }
+}
+
+//register autoloader
+spl_autoload_register("PFS_Autoloader::loadClass");
 
 ?>
--- index.php
+++ index.php
@@ -5,4 +5,10 @@
  * You dont need this file in production!
  */
 
+//define ROOT_PATH
+define('DTU_ROOT_PATH', dirname(__FILE__) . "/");
+
+//add autoloader
+require("autoloader.php");
+
 ?>
